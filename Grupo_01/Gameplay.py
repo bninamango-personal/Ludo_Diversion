64afbb61510ff912f423b5d7616125bfedb8c935
Bryannsss <bninamango.personal@gmail.com>
2023-06-06 20:18:44 -0500

    Update Gameplay.py

--- Grupo_01/Gameplay.py
+++ Grupo_01/Gameplay.py
@@ -1,33 +1,33 @@
 # Logica del tablero
 import random
 
-board = []
-width = height = 0
-
-
-def InitializeBoard(w: int, h: int):
-    global width, height
-    width = w
-    height = h
-    for i in range(width):
-        board.append([' '] * height)
-        for j in range(height):
-            if j == 0:
-                board[i][j] = str(i + 1)
-
-
-def UpdateBoard():
-    for i in range(width):
-        for j in range(height):
-            if j == 0:
-                board[i][j] = str(i + 1)
-            else:
-                board[i][j] = ' '
+board = [
+    ["20", "META"],
+    ["19", " "],
+    ["18", " "],
+    ["17", " "],
+    ["16", " "],
+    ["15", " "],
+    ["14", "BONUS"],
+    ["13", " "],
+    ["12", " "],
+    ["11", " "],
+    ["10", " "],
+    ["09", " "],
+    ["08", "BONUS"],
+    ["07", " "],
+    ["06", " "],
+    ["05", " "],
+    ["04", " "],
+    ["03", " "],
+    ["02", " "],
+    ["01", " "],
+    ["INICIO", " "]]
 
 
 def RenderBoard():
-    for i in range(width):
-        for j in range(height):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
             print(board[i][j], end=" ")
         print()
 
@@ -37,67 +37,116 @@ def GetValue(x: int, y: int) -> str:
 
 
 # Logica del player
-position_x = position_y = 0
-name = ""
+p1_name = ""
+p1_x = p1_y = 0
+
+p2_name = ""
+p2_x = p2_y = 0
 
 
-def InitializePlayer(x: int, y: int, c: str):
-    global position_x, position_y, name
-    position_x = x
-    position_y = y
-    name = c
-    board[position_y][position_x] = name
+def InitializePlayer(x: int, y: int, p1: str, p2: str):
+    global p1_name, p1_x, p1_y
+    global p2_name, p2_x, p2_y
+    p1_x = p2_x = x
+    p1_y = p2_y = y
+    p1_name = p1
+    p2_name = p2
+    board[p1_y][p1_x] = f"{p1_name} {p2_name}"
 
 
-def MovePlayer(y: int):
-    global position_y
-    position_y -= y
+# def GetValue(x: int, y: int) -> str:
+#     global position_x, position_y
+#     position_x = x
+#     position_y = y
+#     return board[position_y][position_x]
 
 
-def GetValue(y: int) -> str:
-    global position_y
-    position_y = y
-    return board[position_y][position_x]
+def MovePlayer_P1(y: int):
+    global p1_x, p1_y
+    board[p1_y][p1_x] = ' '
+    p1_y -= y
+
+
+def MovePlayer_P2(y: int):
+    global p2_x, p2_y
+    board[p2_y][p2_x] = ' '
+    p2_y -= y
 
 
 def UpdatePlayer():
-    global position_x, position_y, name
-    if position_y < 0:
-        position_y = abs(position_y) - 1
-    board[position_y][position_x] = name
+    # if y < 0:
+    #     y = abs(y) - 1
+    # board[y][x] = c
+    global p1_x, p1_y, p1_name
+    global p2_x, p2_y, p2_name
+    global turn
+
+    if turn % 2 == 0:
+        if p1_y < 0:
+            p1_y = abs(p1_y) - 1
+    else:
+        if p2_y < 0:
+            p2_y = abs(p2_y) - 1
+
+    board[p1_y][p1_x] = p1_name
+    board[p2_y][p2_x] = p2_name
+
+    # if
+    # global position_x, position_y, p1_name, p2_name
+    # player = ""
+    # if turn % 2 == 0:
+    #     player = p1_name
+    # else:
+    #     player = p2_name
+    # if position_y < 0:
+    #     position_y = abs(position_y) - 1
+    # board[position_y][position_x] = player
 
 
 # Dado
-def Launch(steps: str) -> str:
-    steps = steps.lower()
-    if steps == "debil":
+def Launch(result: str) -> str:
+    result = result.lower()
+    if result == "debil":
         return random.randint(1, 3)
-    elif steps == "normal":
+    elif result == "normal":
         return random.randint(1, 6)
-    elif steps == "fuerte":
+    elif result == "fuerte":
         return random.randint(4, 6)
+    elif result == 'one':
+        return 1
     return 0
 
 
 # Game Loop
-force: int = 0
 isGameOver: bool = False
+turn = force = 0
 
 
 def Start():
-    InitializeBoard(20, 2)
-    InitializePlayer(1, 19, 'Bryan')
+    InitializePlayer(1, 20, 'Bryan', 'Bryannsss')
     RenderBoard()
 
 
 def Input():
-    global force
-    force = Launch(input("Current force: "))
+    global turn, force
+
+    if turn % 2 == 0:
+        print("# Turno del jugador 1 #")
+    else:
+        print("# Turno del jugador 2 #")
+
+    force = Launch(input("Ingrese el tipo de tiro que desea realizar: "))
+    print(f"DADO: Obtuvo el valor de {force}")
 
 
 def Update():
-    MovePlayer(force)
-    UpdateBoard()
+    global turn, force
+
+    if turn % 2 == 0:
+        MovePlayer_P1(force)
+    else:
+        MovePlayer_P2(force)
+
     UpdatePlayer()
 
 
@@ -110,3 +159,4 @@ while not isGameOver:
     Input()
     Update()
     Render()
+    turn += 1
