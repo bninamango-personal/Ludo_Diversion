7d1d3ce4b86b0aa14f4530f3300e6f449dab8443
Bryannsss <bninamango.personal@gmail.com>
2023-06-09 16:12:14 -0500

    Final

--- main.py
+++ main.py
@@ -1,8 +1,59 @@
-# if __name__ == '__main__':
-#     print('PyCharm')
+import Gameplay_PVP as pvp
+import Gameplay_PVC as pvc
 
-# from Grupo_01 import Dado as dado
-#
-# force = input("Fuerza del dado: ")
-#
-# print("El dado dio: ", dado.Launch(force))
+
+def Select(option: str) -> int:
+    print("#" * 20, end="\n")
+    return int(option)
+
+
+def Start():
+    print()
+    print('## MENU DE INICIO ##')
+    print('1. Empezar el juego')
+    print('2. Record')
+    print('3. Salir')
+
+    option = Select(input("Seleccione una opcion: "))
+
+    print()
+
+    if option == 1:
+        Gameplay()
+    elif option == 2:
+        Record()
+    elif option == 3:
+        Quit()
+
+
+def Gameplay():
+    print('## TIPO DE JUEGO ##')
+    print('1. 1 VS 2')
+    print('2. 1 VS CPU')
+    print("3. Retroceder")
+    print("#" * 20)
+
+    option = Select(input("Seleccione una opcion: "))
+
+    if option == 1:
+        pvp.Game_Loop()
+    elif option == 2:
+        pvc.Game_Loop()
+    elif option == 3:
+        Start()
+
+
+def Record():
+    print("Your record ...")
+
+    input("Presione cualquier tecla para retroceder: ")
+
+    Start()
+
+
+def Quit():
+    print()
+
+
+if __name__ == '__main__':
+    Start()
