07ddc190a15dba8de568fb66fe08d34b48a4f126
AntonioCot <antoniocr4jr@gmail.com>
2023-06-08 22:07:58 -0500

    hola
    a

--- Grupo_02/main.py
+++ Grupo_02/main.py
@@ -21,17 +21,24 @@ if opcion1==1:
         print('2. 1 VS CPU')
     vs()
     vsCPU()
-    print('####################')
     opcion2 = int(input('Seleccione una opcion: '))
 
-print('')
-if opcion2==1:
-    print('## Jugadores ##')
-    player1=input('Ingrese el nombre del jugador 1: ')
-    player2=input('ingrese el nombre del jugador 2: ')
-    print('####################')
+    print('')
+    if opcion2 == 1:
+        print('## Jugadores ##')
+        player1 = input('Ingrese el nombre del jugador 1: ')
+        player2 = input('ingrese el nombre del jugador 2: ')
+        print('####################')
+    elif opcion2 == 2:
+        print('## Jugadores ##')
+        player1 = input('Ingrese el nombre del jugador 1: ')
+        print('Jugador 2: CPU')
+        print('####################')
 
-print('')
-print('## JUEGO ##')
-print('# Ludo divertido #')
-# Aca ya empezaria el codigo del juego
+    print('')
+    print('## JUEGO ##')
+    print('# Ludo divertido #')
+elif opcion1==2:
+    print('Record')
+elif opcion1==3:
+    print('Gracias por ingresar')
